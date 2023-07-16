a37d29adbacfdb25680d5cc2bef650e04465cb1e
EngrArfin <arfin.cse.green.edu.bd.gmail.com>
2023-07-17 04:06:45 +0600

    up to date all

--- index.js
+++ index.js
@@ -12,7 +12,6 @@ app.use(express.json());
 //coffeeMaster
 //2kyvBw6DDxaiLx43
 
-
 console.log(process.env.DB_USER)
 console.log(process.env.DB_PASS)
 
@@ -33,17 +32,33 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    const coffeeCollection = client.db('coffeeDB').collection('coffee')
+
+
+    app.get('/coffee', async(req, res) => {
+      const cursor = coffeeCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+      
+    })
+
+    app.post('/coffee', async(req, res)=>{
+      const newCoffee = req.body;
+      console.log(newCoffee);  
+      const result = await coffeeCollection.insertOne(newCoffee);
+      res.send(result);
+    })
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    /* await client.close(); */
   }
 }
 run().catch(console.dir);
-
-
 
 app.get('/', (req, res) => {
     res.send('Coffee making server is running');
